b78173e081a843e0ad87e9031c8d1e534a53ffc7
anhZK2420 <anhcuto2005@gmail.com>
2025-06-05 14:44:13 +0700

    test mini project with tailwind

---
+++ testwithtailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./*.html", "./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      spacing: {
+        35: "35.5px",
+      },
+      colors: {
+        gray33: "#333",
+        orangefa: "#ffa400",
+      },
+    },
+  },
+  plugins: [],
+};
